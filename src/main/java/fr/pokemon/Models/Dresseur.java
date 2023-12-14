0391670c34bd51716069351777be47aeeec5e5d1
NathanManiezPro <Nathanmaniez@gmail.com>
2023-12-14 16:39:36 +0100

    Ajout de dresseur

---
+++ src/main/java/fr/pokemon/Models/Dresseur.java
@@ -0,0 +1,28 @@
+package fr.pokemon.Models;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Dresseur {
+    public Pokeball pokeball;
+    public String name;
+    public List listePokeball;
+
+    public Dresseur(String name, List listePokeball, Pokeball pokeball){
+        this.name = name;
+        this.listePokeball = listePokeball;
+        this.pokeball = pokeball;
+    }
+
+    public String attackNamePokemon() {
+
+        return null;
+    }
+
+    public void selectPokemon() {
+
+    }
+
+
+
+}
