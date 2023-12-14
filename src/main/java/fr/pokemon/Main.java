f5031ba3324700af83a572b88e3de02b454dca7d
KoWyNx <fberrani@hotmail.fr>
2023-12-14 16:58:04 +0100

    test main

--- src/main/java/fr/pokemon/Main.java
+++ src/main/java/fr/pokemon/Main.java
@@ -1,9 +1,31 @@
 package fr.pokemon;
 
 
+import fr.pokemon.Models.Pokeball;
+import fr.pokemon.Models.Pokemon;
+
 public class Main {
 
     public static void main(String[] args){
+        Pokemon pokemon = new Pokemon(
+                "carapuce",
+                100,
+                80,
+                20,
+                false,
+                "normal",
+                "feu"
+        );
 
+        Pokeball pokeball = new Pokeball(pokemon);
+
+        System.out.println(pokeball.isPokeballEmpty());
+
+
+        System.out.println(pokemon);
     }
+
+
+
+
 }
